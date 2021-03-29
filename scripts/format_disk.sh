204f42a20ac3625f00f45d836643fe6d23cfb6c8
Jim Howard <bit@bintec.com>
2021-03-28 21:58:56 -0400

    Update format_disk.sh
    Also erase the backup GPT table or hardware RAID information at the end of the disk.

--- scripts/format_disk.sh
+++ scripts/format_disk.sh
@@ -2,9 +2,16 @@
 
 DISK=$1;
 
-echo -n "Formatting disk ${DISK}..."
+getmsize() { echo $(expr $(diskinfo -v $1 | grep bytes | cut -d \# -f 1) / 1024 / 1024  ) ; }
+
+DISKSIZE=$( getmsize ${DISK} );
+
+echo  "Erasing partitions on disk ${DISK}...${DISKSIZE}M"
 /bin/dd if=/dev/zero of=/dev/${DISK} bs=1m count=1
 RV=$?
+
+#echo  "Erasing backup GPT on disk ${DISK}...${DISKSIZE}M"
+/bin/dd if=/dev/zero of=/dev/${DISK} bs=1m seek=$((${DISKSIZE}-2))
 
 if [ "$RV" = "0" ]
 then
